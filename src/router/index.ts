e522382ec02da1e13a574e7cd33a7690ed880759
unknown <2478731248@11.com>
2023-01-10 10:03:00 +0800

    初次提交

---
+++ src/router/index.ts
@@ -0,0 +1,13 @@
+import { createRouter, createWebHashHistory, RouteRecordRaw } from 'vue-router'
+import HomeView from '../views/HomeView.vue'
+
+const routes: Array<RouteRecordRaw> = [
+
+]
+
+const router = createRouter({
+  history: createWebHashHistory(),
+  routes
+})
+
+export default router
